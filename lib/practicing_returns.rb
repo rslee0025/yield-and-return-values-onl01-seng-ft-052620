9dcbf293c7db308428855cf2b9ffb7d27021fda2
Rachel S. Lee <rslee0025@gmail.com>
2020-05-31 20:12:10 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -12,12 +12,3 @@ end
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 
-
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-end
